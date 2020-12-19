9ced7d40ec3dd6119e2591675f068c6f4534c5d3
117503445 <t117503445@gmail.com>
2020-12-19 15:46:38 +0800

    0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="htutil",
-    version="0.0.3",
+    version="0.0.4",
     author="117503445",
     author_email="t117503445@gmail.com",
     description="HaoTian's Python Util",
