2574a68c698236a6d18f2973101f1312e13f3b67
117503445 <t117503445@gmail.com>
2021-04-20 18:39:29 +0800

    fix: update setup ver
    0.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="htutil",
-    version="0.1.7",
+    version="0.1.8",
     author="117503445",
     author_email="t117503445@gmail.com",
     description="HaoTian's Python Util",
